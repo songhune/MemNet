95a8f6a770bc8a96b57959fc8be4c31c28156e2e
songhune <songhune@ajou.ac.kr>
2018-01-29 17:12:44 +0900

    testings for changing local-remote

--- data_utils.py
+++ data_utils.py
@@ -4,6 +4,7 @@ import os
 import re
 import numpy as np
 
+
 def load_task(data_dir, task_id, only_supporting=False):
     '''Load the nth task. There are 20 tasks in total.
 
@@ -20,12 +21,13 @@ def load_task(data_dir, task_id, only_supporting=False):
     test_data = get_stories(test_file, only_supporting)
     return train_data, test_data
 
+
 def tokenize(sent):
     '''Return the tokens of a sentence including punctuation.
     >>> tokenize('Bob dropped the apple. Where is the apple?')
     ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
     '''
-    return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
+    return [x.strip() for x in re.split('(\\W+)?', sent) if x.strip()]
 
 
 def parse_stories(lines, only_supporting=False):
@@ -35,16 +37,17 @@ def parse_stories(lines, only_supporting=False):
     data = []
     story = []
     for line in lines:
-        line = str.lower(line)
+        line = str.lower(line)  # 다 소문자로 바꾼다
         nid, line = line.split(' ', 1)
         nid = int(nid)
         if nid == 1:
             story = []
-        if '\t' in line: # question
-            q, a, supporting = line.split('\t')
-            q = tokenize(q)
-            #a = tokenize(a)
-            # answer is one vocab word even if it's actually multiple words
+            # bAbI 특성상 연관된 문장이 끝나면 다시 1번부터 번호를 매기기 때문에 이런 식으로 구분해준것
+        if '\t' in line:  # question이라면
+            q, a, supporting = line.split('\t')  # 나누고
+            q = tokenize(q)  # 쪼갠다
+            # a = tokenize(a)
+            # answer is one vocab word even if it's actually multiple words??? 이게 뭐지?
             a = [a]
             substory = None
 
@@ -62,7 +65,7 @@ def parse_stories(lines, only_supporting=False):
 
             data.append((substory, q, a))
             story.append('')
-        else: # regular sentence
+        else:  # regular sentence
             # remove periods
             sent = tokenize(line)
             if sent[-1] == ".":
@@ -78,15 +81,17 @@ def get_stories(f, only_supporting=False):
     with open(f) as f:
         return parse_stories(f.readlines(), only_supporting=only_supporting)
 
-def vectorize_data(data, word_idx, sentence_size, memory_size):
+
+def vectorize_data(data, word_idx, sentence_size, memory_size, max_story_size):
     """
     Vectorize stories and queries.
 
     If a sentence length < sentence_size, the sentence will be padded with 0's.
-
+         문장의 길이        문장의 크기, 즉 변수
     If a story length < memory_size, the story will be padded with empty memories.
     Empty memories are 1-D arrays of length sentence_size filled with 0's.
 
+    songhune edited: if a story length > memory size, max size of each story will be returned.
     The answer array is returned as a one-hot encoding.
     """
     S = []
@@ -95,30 +100,46 @@ def vectorize_data(data, word_idx, sentence_size, memory_size):
     for story, query, answer in data:
         ss = []
         for i, sentence in enumerate(story, 1):
+            # 1부터 시작한다. #ls는 sentence size(7개)에서 현재 문장이 들어가 있는 만큼의 공간을 빼줌
             ls = max(0, sentence_size - len(sentence))
-            ss.append([word_idx[w] for w in sentence] + [0] * ls)
+            ss.append([word_idx[w] for w in sentence] + [0] * ls)  # 뭘 붙이냐 하면 word index를 붙인다.
 
         # take only the most recent sentences that fit in memory
-        ss = ss[::-1][:memory_size][::-1]
+        '''일단 전체 메모리를 뱉어내게 한다. '''
+        ss = ss[::-1][:max_story_size][::-1]
+        # 역수로 출력 / 전체 크기만큼 / 역수로 출력
+        '''
+        if story_size<memory_size
+            ss =[::-1][:memory_size][::-1]
 
-        # Make the last word of each sentence the time 'word' which 
-        # corresponds to vector of lookup table
+        elif story_size>memory_size
+            if memory full
+            ss = 기존
+        이럴 필요가 있는가?
+        '''
+        # Make the last word of each sentence the time 'word' which corresponds to vector of lookup table
         for i in range(len(ss)):
-            ss[i][-1] = len(word_idx) - memory_size - i + len(ss)
+            # 30에서 메모리 사이즈 10개 빼고 i만큼 빼고, ss의 길이를 붙인다. 룩업 테이블과 연관됨
+            ss[i][-1] = len(word_idx) - max_story_size - i + len(ss)
 
-        # pad to memory_size
-        lm = max(0, memory_size - len(ss))
+        # pad  '0' to memory_size
+        # songhune edited: 메모리 사이즈에서 story size를 빼는 것이 아니라 전체 크기에서 메모리 사이즈를 반환한다.
+        lm = max(0, max_story_size - len(ss))
         for _ in range(lm):
             ss.append([0] * sentence_size)
 
+        # manipulate query
         lq = max(0, sentence_size - len(query))
         q = [word_idx[w] for w in query] + [0] * lq
 
-        y = np.zeros(len(word_idx) + 1) # 0 is reserved for nil word
+        y = np.zeros(len(word_idx) + 1)  # 0 is reserved for nil word
         for a in answer:
             y[word_idx[a]] = 1
 
         S.append(ss)
         Q.append(q)
         A.append(y)
+    print(np.array(S).shape)
+    print(np.array(Q).shape)
+    print(np.array(A).shape)
     return np.array(S), np.array(Q), np.array(A)
--- single.py
+++ single.py
@@ -4,12 +4,14 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 from data_utils import load_task, vectorize_data
-from sklearn import cross_validation, metrics
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from memn2n import MemN2N
 from itertools import chain
 from six.moves import range, reduce
 
 import tensorflow as tf
+import tensorboard as tb
 import numpy as np
 
 tf.flags.DEFINE_float("learning_rate", 0.01, "Learning rate for SGD.")
@@ -21,8 +23,8 @@ tf.flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
 tf.flags.DEFINE_integer("hops", 3, "Number of hops in the Memory Network.")
 tf.flags.DEFINE_integer("epochs", 100, "Number of epochs to train for.")
 tf.flags.DEFINE_integer("embedding_size", 20, "Embedding size for embedding matrices.")
-tf.flags.DEFINE_integer("memory_size", 50, "Maximum size of memory.")
-tf.flags.DEFINE_integer("task_id", 3, "bAbI task id, 1 <= id <= 20")
+tf.flags.DEFINE_integer("memory_size",5, "Maximum size of memory.")
+tf.flags.DEFINE_integer("task_id",3, "bAbI task id, 1 <= id <= 20")
 tf.flags.DEFINE_integer("random_state", None, "Random state.")
 tf.flags.DEFINE_string("data_dir", "memn2n/data/tasks_1-20_v1-2/en/", "Directory containing bAbI tasks")
 FLAGS = tf.flags.FLAGS
@@ -32,38 +34,57 @@ print("Started Task:", FLAGS.task_id)
 # task data
 train, test = load_task(FLAGS.data_dir, FLAGS.task_id)
 data = train + test
-
+#데이터로부터 사용된 모든 단어를 뽑아내 어순대로 정렬한다.
 vocab = sorted(reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in data)))
+#데이터로부터 뽑아낸 단어에 인덱스를 매긴다.
 word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
 
+#story의 최대 길이는 10개
 max_story_size = max(map(len, (s for s, _, _ in data)))
+#story의 평균 사이즈는 6개
 mean_story_size = int(np.mean([ len(s) for s, _, _ in data ]))
+#story의 문장 길이는 6개
 sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
+#question의 문장 길이는 3개
 query_size = max(map(len, (q for _, q, _ in data)))
+#메모리의 크기는 story의 크기와 50 중에 작은 걸로 정해짐
+#songhune edited: 메모리 자체의 크기를 작게 만듦
 memory_size = min(FLAGS.memory_size, max_story_size)
-
+print('Memory size')
 # Add time words/indexes
 for i in range(memory_size):
     word_idx['time{}'.format(i+1)] = 'time{}'.format(i+1)
 
 vocab_size = len(word_idx) + 1 # +1 for nil word
 sentence_size = max(query_size, sentence_size) # for the position
+#sentence 길이에 time word라는 걸 하나 붙이므로(왜 하나 붙이나? 해결됨, timeword라고 표현할 수 있는 부분은 index 하나만 붙이면 되니까)
 sentence_size += 1  # +1 for time words
-
 print("Longest sentence length", sentence_size)
 print("Longest story length", max_story_size)
 print("Average story length", mean_story_size)
 
 # train/validation/test sets
-S, Q, A = vectorize_data(train, word_idx, sentence_size, memory_size)
-trainS, valS, trainQ, valQ, trainA, valA = cross_validation.train_test_split(S, Q, A, test_size=.1, random_state=FLAGS.random_state)
-testS, testQ, testA = vectorize_data(test, word_idx, sentence_size, memory_size)
+S, Q, A = vectorize_data(train, word_idx, sentence_size, memory_size, max_story_size)
+#songhune edited 맥스 스토리 사이즈를 넣는다
+#trainS, valS, trainQ, valQ, trainA, valA = cross_validation.train_test_split(S, Q, A, test_size=.1, random_state=FLAGS.random_state) #this model has been depricated
 
-print(testS[0])
+#train에서 실제로 트레이닝 할 데이터를 분리한다. ! 얼마나 트레이닝할때 쓸거냐면 90%를 사용할 것이다. 데이터도 셔플한다.
+trainS, valS, trainQ, valQ, trainA, valA = train_test_split(S, Q, A, test_size=.1, random_state=FLAGS.random_state)
+testS, testQ, testA = vectorize_data(test, word_idx, sentence_size, memory_size, max_story_size)
+
+'''
+print("########################################################################")
+for i in range (1000):
+    print('the number of i would be',i,'\n', trainS[i])
+    print()
+    print(trainQ[i])
+    print('###########################')
 
 print("Training set shape", trainS.shape)
+'''
+print("Number of story size of this task",len(S))
 
-# params
+# params, 즉, 전체 개수
 n_train = trainS.shape[0]
 n_test = testS.shape[0]
 n_val = valS.shape[0]
@@ -72,8 +93,10 @@ print("Training Size", n_train)
 print("Validation Size", n_val)
 print("Testing Size", n_test)
 
+#각각의 정답의 위치를 뱉어낸다.
 train_labels = np.argmax(trainA, axis=1)
 test_labels = np.argmax(testA, axis=1)
+#여기까진 trainset에서의 라벨값
 val_labels = np.argmax(valA, axis=1)
 
 tf.set_random_seed(FLAGS.random_state)
@@ -83,8 +106,7 @@ batches = zip(range(0, n_train-batch_size, batch_size), range(batch_size, n_trai
 batches = [(start, end) for start, end in batches]
 
 with tf.Session() as sess:
-    model = MemN2N(batch_size, vocab_size, sentence_size, memory_size, FLAGS.embedding_size, session=sess,
-                   hops=FLAGS.hops, max_grad_norm=FLAGS.max_grad_norm)
+    model = MemN2N(batch_size, vocab_size, sentence_size,memory_size, FLAGS.embedding_size, max_story_size, session=sess, hops=FLAGS.hops, max_grad_norm=FLAGS.max_grad_norm)
     for t in range(1, FLAGS.epochs+1):
         # Stepped learning rate
         if t - 1 <= FLAGS.anneal_stop_epoch:
@@ -114,7 +136,9 @@ with tf.Session() as sess:
             val_preds = model.predict(valS, valQ)
             train_acc = metrics.accuracy_score(np.array(train_preds), train_labels)
             val_acc = metrics.accuracy_score(val_preds, val_labels)
+            summary_op = tf.summary.merge_all()
 
+            file_writer = tf.summary.FileWriter('./logs',sess.graph)
             print('-----------------------')
             print('Epoch', t)
             print('Total Cost:', total_cost)
